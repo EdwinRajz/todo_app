3167aab2c85586f71e8db081cebae508339df765
Edwin Rajz <reiseddy@yahoo.com>
2021-05-26 21:23:31 +0300

    [WIP initial commin]

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-
-
-
 function addTask() {
-  let titleTag = document.createElement("task-title");
-  let testTag = document.createElement("task-text");
-  let timeTag = document.createElement("task-time");
-
-
-
-
-
   let taskContainer = document.createElement("div");
   taskContainer.className = "todo-container";
-  let text = document.createTextNode(createTask.fetchText);
-  taskContainer.appendChild(text);
-  document.getElementById("todo-list").appendChild(taskContainer);
+  let titleTag = document.createElement("h1");
+  titleTag.className = "title";
+  let textTag = document.createElement("h4");
+  textTag.className = "text";
+  let timeTag = document.createElement("p");
+  timeTag.className = "created-at";
+
+  let titleInput = document.querySelector("input-title");
+  let textInput = document.querySelector("input-text");
+
+  console.log(titleInput);
+  titleTag.innerText(titleInput);
+  textTag.innerText(textInput);
+  timeTag = Date.now();
+
+  document
+    .getElementById("todo-list")
+    .appendChild(taskContainer);
 }
