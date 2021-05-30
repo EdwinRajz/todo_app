5e94d943f00ddc79cbb50aa4d70c81a8a932ba1d
Edwin Rajz <reiseddy@yahoo.com>
2021-05-30 23:51:19 +0300

    [WIP] timestamp formatted

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 const titleInput = document.querySelector(".input-title");
 const textInput = document.querySelector(".input-text");
-let taskContainer = document.createElement("div");
-taskContainer.classList.add("todo-container");
-let titleTag = document.createElement("h1");
-titleTag.classList.add("title");
-let textTag = document.createElement("h4");
-textTag.classList.add("text");
-let timeTag = document.createElement("p");
-timeTag.classList.add("created-at");
 
 function addTask() {
+  let taskContainer = document.createElement("div");
+  taskContainer.classList.add("todo-container");
+  let titleTag = document.createElement("h1");
+  titleTag.classList.add("title");
+  let textTag = document.createElement("h4");
+  textTag.classList.add("text");
+  let timeTag = document.createElement("p");
+  timeTag.classList.add("created-at");
+
   titleTag.innerText = titleInput.value;
   textTag.innerText = textInput.value;
-  timeTag.innerText = Date.now();
-
+  time = Date();
+  timeTag.innerText = time.slice(0, 21);
   taskContainer.appendChild(titleTag);
   taskContainer.appendChild(textTag);
   taskContainer.appendChild(timeTag);
